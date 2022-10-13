ff16a9396785d1afd18067a16bb3d25905f17d04
Sirtankyou <78551443+Sirtankyou@users.noreply.github.com>
2022-10-13 23:42:01 +0100

    Add files via upload

---
+++ Console Genetic Algorithm/Console Genetic Algorithm/Console Genetic Algorithm.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <list>
+#include "Individual.h"
+#include "Population.h"
+using namespace std;
+
+void popTest();
+
+
+int main()
+{
+	popTest();
+	// List of objects 
+	// Weight of bag
+	// Chromosome links to items in bag. 1 in bag 0 out of bag
+	// If total weight > bag weight then fitness is 0
+
+}
+
+
